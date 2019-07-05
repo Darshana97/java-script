c73b66f2a31e47d927b6433c79f4a75b6f285ebb
Darshana97 <dpbrayan1@gmail.com>
2019-07-05 16:19:19 +0530

    java script

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log('hello achi');
+console.log(1000);
+console.log(true);
+
+var name = "darshana";
+console.log(name);
+
+console.log([1,2,3,4]);
+
+console.log({a:1, b:2});
+
+console.table({a:1, b:2});
+
+console.error('this is some error');
+
+console.clear();
+
+console.warn('this is a warning');
+
+console.time('heloooo');
+
+    console.log('hello achi');
+    console.log('hello achi');
+    console.log('hello achi');
+    console.log('hello achi');
+    console.log('hello achi');
+    console.log('hello achi');
+    console.log('hello achi');
+    console.log('hello achi');
+    console.log('hello achi');
+    console.log('hello achi');
+
+console.timeEnd('heloooo');
+
