5d738996bfb4f3ac582d5ba371c9269f5bcb66d1
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-07-07 00:40:19 +0530

    data-types
    data-types in js

---
+++ data-types.js
@@ -0,0 +1,37 @@
+//PRIMITIVE
+
+
+//String
+const name = 'darshana seneviratana';
+
+//Number
+const age = 20;
+
+//Boolean
+const gf = true;
+
+//Null
+const car = null;
+
+//Undefined
+let test;
+
+//Symbol
+const sym = Symbol();
+
+
+//REFERENCE TYPES - Objects
+
+//Array
+const hobbies = ['movies', 'music'];
+
+//Object literal
+const address = {
+    city: 'bostan',
+    state: 'MA'
+}
+
+const today = new Date();
+console.log(today);
+
+console.log(typeof address);
