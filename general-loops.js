33782aeba97638c73b7ba9cf4c115014bfb45bd4
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-07-11 01:04:57 +0530

    general-loops
    general-loops in js

---
+++ general-loops.js
@@ -0,0 +1,15 @@
+// FOR LOOP
+
+for(let i = 0; i < 10; i++){
+
+    if(i === 2){
+        console.log('2 is my favorite number');
+        continue;
+    }
+
+    if(i === 5){
+        console.log('Stop the loop');
+        break;
+    }
+    console.log('Number ' + i);
+}
