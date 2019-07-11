72915afc2c8dddbbdd1c8abb98d80b426d584202
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-07-12 00:27:47 +0530

    if
    if in js

---
+++ if.js
@@ -0,0 +1,36 @@
+const id = '100';
+
+// //EQUAL TO
+// if(id == 100){
+//     console.log('CORRECT');
+// }else{
+//     console.log('INCORRECT');
+// }
+
+// //Not EQUAL TO
+// if(id != 1000){
+//     console.log('CORRECT');
+// }else{
+//     console.log('INCORRECT');
+// }
+
+// //EQUAL TO VALUE & TYPE
+// if(id === 100){
+//     console.log('CORRECT');
+// }else{
+//     console.log('INCORRECT');
+// } 
+
+// //EQUAL TO VALUE & TYPE
+// if(id !== 100){
+//     console.log('CORRECT');
+// }else{
+//     console.log('INCORRECT');
+// } 
+
+if(id){
+    console.log(`The ID is ${id}`);
+}else{
+    console.log('NO ID');
+}
+
