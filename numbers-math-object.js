02225a200eaf17e285472e7867a17241aac9d255
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-07-13 00:45:59 +0530

    numbers-math-object
    numbers-math-object in js

---
+++ numbers-math-object.js
@@ -0,0 +1,28 @@
+const num1 = 100;
+const num2 = 60;
+let val;
+
+//simple math wirh number
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+//Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.5);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-356);
+val = Math.pow(3, 4);
+val = Math.min(2,34,56,1,0,-12);
+val = Math.max(-45, 78, 99,100,43);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1);
+
+
+console.log(val);
