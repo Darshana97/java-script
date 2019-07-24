ac808c266aa7415a350d1c589420a31e4507b98c
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-07-24 18:33:54 +0530

    variable
    variable

---
+++ variable.js
@@ -0,0 +1,57 @@
+// //var,let,const
+
+// var name = 'darshana senevirathana';
+// console.log(name);
+// name = 'achi senevirathna';
+// console.log(name);
+
+// //init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'heloooo';
+// console.log(greeting); 
+
+// //letters, numbers, _, $
+// //can not start with number
+
+// //multi word vars
+// var firstName = 'pubudu'; //camel case 
+// var first_name = 'bandara'; //underscore
+// var FirstName = 'brayan'; //pascal case
+// var firstname;
+ 
+// //LET
+// let name;
+// name = 'darshana senevirathana';
+// console.log(name);
+// name = 'achi senevirathna';
+// console.log(name);
+
+
+// //CONST
+// const name = 'darshana';
+// console.log(name);
+// //can not reasign
+// name = 'achi';
+// console.log(name);
+
+
+// //have to assign a value
+// const greeting;
+
+
+// const person = {
+//     name: 'achi',
+//     age: 22
+// }
+
+// person.name = 'brayan';
+// person.age = 21;
+
+// console.log(person);
+
+
+const numbers = [1,2,3,4,5];
+numbers.push(10);
+
+console.log(numbers);
